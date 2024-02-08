d92800752a696aadaf77831f96a99ba886885d5b
Shivhare <AL76295@ad.wellpoint.com>
2024-02-08 18:25:34 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Logo from '../assets/pizzaLogo.png';
+import {Link} from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import '../styles/Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='leftSide'>
+        <img src={Logo} />
+        <div className='hiddenLinks'>
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        </div>
+      </div>
+      <div className='rightSide'>
+      <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <button>
+            <ReorderIcon />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
